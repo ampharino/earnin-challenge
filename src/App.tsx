c33f77ff6677eced121ec4960c2893e716dd6360
Ampharin Ongvises <ampharin@gmail.com>
2025-01-18 13:43:04 +0700

    chore(setup): Cleanup files from vite scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import Table from './Table';
 import Button from './components/Button';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <section className="p-6">
